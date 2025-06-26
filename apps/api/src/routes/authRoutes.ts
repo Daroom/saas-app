8dfa1d345dda0c0ef3edb5e1b1fb8554723120a2
Morad MAWHOUB <moradmawhoub@192.168.11.102>
2025-06-26 06:06:10 +0100

    init react app and setup its config

--- apps/api/src/routes/authRoutes.ts
+++ apps/api/src/routes/authRoutes.ts
@@ -1,14 +1,14 @@
 import { Router } from 'express';
-import * as authController from '../controllers/authController';
+import { login, register } from '../controllers/authController';
 import { authenticateJWT } from '../middlewares/authMiddleware';
 
 const router = Router();
 
-router.post('/register', authController.register);
-router.post('/login', authController.login);
+router.post('/register', register);
+router.post('/login', login);
 
 
-router.get('/profile', authenticateJWT, (req, res, next) => {
+router.get('/profile', authenticateJWT, (req, res) => {
   res.json({user:req.user});
 });
 
