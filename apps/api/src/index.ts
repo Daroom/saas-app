49f3cef2ed67cb135486cf54b9ba3c3a06e97466
Morad MAWHOUB <moradmawhoub@192.168.3.13>
2025-07-11 22:55:28 +0100

    make unused variable ts error loose

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,7 @@
 import app from './app.js';
 import { db } from '@saas-app/database';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, async () => {
   console.log(`Server is ruuuunning on port ${PORT}`);
